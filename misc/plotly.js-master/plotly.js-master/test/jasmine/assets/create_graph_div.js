35e6bff554140c196e632babeb8bdf90686fc917
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-24 21:10:30 +0800

    Delete misc/plotly.js-master/plotly.js-master directory

--- misc/plotly.js-master/plotly.js-master/test/jasmine/assets/create_graph_div.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function createGraphDiv() {
-    var gd = document.createElement('div');
-    gd.id = 'graph';
-    document.body.appendChild(gd);
-    return gd;
-};
--- misc/plotly.js-master/plotly.js-master/test/jasmine/assets/destroy_graph_div.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = function destroyGraphDiv() {
-    var gd = document.getElementById('graph');
-    document.body.removeChild(gd);
-};
